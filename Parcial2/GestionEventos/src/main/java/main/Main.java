db1ee876800c6f33018e5f24f5e0344d2cecec9c
ErwinMH <2190017@ipisa.edu.do>
2026-03-07 17:27:14 -0400

    Se creó el endpoint para /registrar

--- Parcial2/GestionEventos/src/main/java/main/Main.java
+++ Parcial2/GestionEventos/src/main/java/main/Main.java
@@ -3,8 +3,12 @@ package main;
 import io.javalin.Javalin;
 import io.javalin.http.staticfiles.Location;
 import main.controllers.UsuarioController;
+import main.models.Rol;
+import main.models.Usuario;
 import main.services.BootStrapServices;
 import io.javalin.rendering.template.JavalinThymeleaf;
+import main.services.GestionDb;
+import main.services.UsuarioService;
 
 public class Main {
     public static void main(String[] args){
@@ -23,6 +27,39 @@ public class Main {
             });
 
             config.routes.get("/usuarios", UsuarioController::listar);
+
+            //Los usuarios se crean como PARTICIPANTES, el ADMIN decide si convertirlos en ORGANIZADOR
+            config.routes.post("/registrar", ctx -> {
+
+                String username = ctx.formParam("username");
+                String email = ctx.formParam("email");
+                String password = ctx.formParam("password");
+
+                UsuarioService usuarioService = UsuarioService.getInstancia();
+                GestionDb<Rol> rolDb = new GestionDb<>(Rol.class);
+
+                Rol rolParticipante = null;
+
+                for(Rol r : rolDb.findAll()){
+                    if(r.getRol().equals("Participante")){
+                        rolParticipante = r;
+                        break;
+                    }
+                }
+
+                Usuario usuario = new Usuario(
+                        username,
+                        email,
+                        password,
+                        true,
+                        rolParticipante
+                );
+
+                usuarioService.crear(usuario);
+
+                ctx.redirect("/login");
+            });
+
         });
 
         BootStrapServices.getInstancia().init();
