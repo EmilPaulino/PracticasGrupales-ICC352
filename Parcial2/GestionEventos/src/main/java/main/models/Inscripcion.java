41b1bf2324e029cab709d2326dcb3b520a570a08
ErwinMH <2190017@ipisa.edu.do>
2026-03-13 13:41:04 -0400

    Se agregó el campo asistió para confirmar la asistencia con QR

--- Parcial2/GestionEventos/src/main/java/main/models/Inscripcion.java
+++ Parcial2/GestionEventos/src/main/java/main/models/Inscripcion.java
@@ -1,19 +1,28 @@
 package main.models;
 
 import jakarta.persistence.*;
+import main.services.GestionDb;
+
 import java.time.*;
 
 @Entity
 @Table(uniqueConstraints = @UniqueConstraint(columnNames = {"usuario_id","evento_id"})) //Para que no se repitan inscripciones
 public class Inscripcion {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private LocalDate fecha;
     private LocalTime hora;
+
     private String qrToken;
+
+    private boolean asistio; // Para confirmar asistencia
+
     @ManyToOne
     private Usuario usuario;
+
     @ManyToOne
     private Evento evento;
 
@@ -21,12 +30,13 @@ public class Inscripcion {
 
     }
 
-    public Inscripcion(LocalDate fecha,  LocalTime hora, String qrToken, Usuario usuario, Evento evento) {
+    public Inscripcion(LocalDate fecha, LocalTime hora, String qrToken, Usuario usuario, Evento evento) {
         this.fecha = fecha;
         this.hora = hora;
         this.qrToken = qrToken;
         this.usuario = usuario;
         this.evento = evento;
+        this.asistio = false; // nadie ha asistido cuando se inscribe
     }
 
     public Long getId() {
@@ -61,6 +71,14 @@ public class Inscripcion {
         this.qrToken = qrToken;
     }
 
+    public boolean isAsistio() {
+        return asistio;
+    }
+
+    public void setAsistio(boolean asistio) {
+        this.asistio = asistio;
+    }
+
     public Usuario getUsuario() {
         return usuario;
     }
@@ -76,4 +94,5 @@ public class Inscripcion {
     public void setEvento(Evento evento) {
         this.evento = evento;
     }
+
 }
